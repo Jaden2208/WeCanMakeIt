61c8efd0542041d46996ddf680c02b1011017023
ryu22 <ryu2208@naver.com>
2020-04-21 16:24:08 +0900

    GroupRoomActivity UI만 간단하게.

---
+++ app/src/main/java/com/whalez/wecanmakeit/ui/main/group/GroupRoomActivity.kt
@@ -0,0 +1,13 @@
+package com.whalez.wecanmakeit.ui.main.group
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.whalez.wecanmakeit.R
+
+class GroupRoomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_room)
+    }
+}
